030185a3c0c53e9be4cc20cb69cd9f0bfd2909d1
Milena-Dias29 <cleliamilena29@gmail.com>
2026-07-25 16:59:54 -0300

    feat: inicio do projeto flask e criação de template filmes

--- pip.py
+++ pip.py
@@ -2,11 +2,19 @@ from flask import Flask, render_template
 
 #inicializo o Flask
 app = Flask ("Servidor")
-
+testes = ["Batman", "Barbie", "Senhor dos anéis", "O hobbit", "Era do gelo", "Shrek"]
+#Criar dicionario, como filmes, genero e diretores, etc...
 # Defino rotas e funções
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+@app.route("/teste")
+def teste():
+    lista = f""
+    for f in testes:
+        lista += f"{f}, "
+    return lista
 
 #Rodo o Servidor
 
