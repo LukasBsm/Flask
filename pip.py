3e952366775ec5271d3b0888c149fec8b8f080a7
Milena-Dias29 <cleliamilena29@gmail.com>
2026-08-01 16:56:11 -0300

    Att3

--- pip.py
+++ pip.py
@@ -1,21 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 #inicializo o Flask
 app = Flask ("Servidor")
-testes = ["Batman", "Barbie", "Senhor dos anéis", "O hobbit", "Era do gelo", "Shrek"]
+lista_filme = ["Batman", "Barbie", "Senhor dos anéis", "O hobbit", "Era do gelo", "Shrek", "Avatar", "Homem Aranha"]
 #Criar dicionario, como filmes, genero e diretores, etc...
 # Defino rotas e funções
+#Adcionar dicionario de filmes, com nome diretor, etc
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
-@app.route("/teste")
-def teste():
+@app.route("/filmes")
+def filme():
     lista = f""
-    for f in testes:
+    for f in lista_filme:
         lista += f"{f}, "
-    return lista
+    return ", ".join(lista_filme) + "."
 
+@app.route("/ver_filmes")
+def ver_filmes():
+    return render_template("filmes.html", lista_filme = lista_filme)
+
+@app.route ("/add_filme", methods =["POST"])
+def add_filme():
+    filme = request.form['titulo']
+    if len(filme) > 3:
+        lista_filme.append(filme)
+    return redirect ("/ver_filmes")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("filmes")
 #Rodo o Servidor
 
 app.run(debug=True)
